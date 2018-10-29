71ff005509acc1b93b6d6e99eed646313a7da55c
Yen Yin Hing <yenyinf287@gmail.com>
2018-10-28 18:23:37 -0700

    scrolling at the right place

--- js/aloha.js
+++ js/aloha.js
@@ -5,11 +5,12 @@ $(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
       || location.hostname == this.hostname) {
 
+      var headerHeight = $('header').outerHeight();
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - headerHeight
         }, 1000);
         return false;
       }
