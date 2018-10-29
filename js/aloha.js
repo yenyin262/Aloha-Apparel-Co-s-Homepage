16d7b29ccc2c896d6b471db3694ad70461cd87ec
Yen Yin Hing <yenyinf287@gmail.com>
2018-10-29 13:15:37 -0700

    aloha project 29.10.18

--- js/aloha.js
+++ js/aloha.js
@@ -1,6 +1,5 @@
 $(function () {
-  // your jQuery code here...
-
+ 
   $('a[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
       || location.hostname == this.hostname) {
@@ -18,7 +17,6 @@ $(function () {
   });
 
   $('.main-carousel').flickity({
-    // options
     cellAlign: 'left',
     contain: true,
     prevNextButtons: false,
@@ -32,6 +30,7 @@ $(function () {
   $('.addcartbutton').on('click', function () {
 
     counter++;
+    
     if (counter > 0) {
       $('.cartcontainer span').html('<span class="number-cart">' + counter + '</span>');
     }
@@ -54,49 +53,6 @@ $(function () {
   });
 
 });
-
-//   function validemailadd(){
-// var emailaddress = document.getElementById('emailaddress').value; 
-
-// var validemail = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
-
-//   if (emailaddress.match(validemail))
-//   {
-//     alert("Thanks for subscribing!");
-//    return (true)
-
-//   }
-//     alert("Enter a valid email address!")
-//     return (false)
-// }
-
-
-//   $('.main-carousel').flickity({
-//     // options
-//     cellAlign: 'left',
-//     contain: true,
-//     prevNextButtons: false
-//   });
-
-
-
-//$(".submitform".val(),.email: $("input[name=email]").val(), 
-  //$(".subscribebutton").on("click", function() {
-
-
-   // alert("Thanks for subscribing!");
-  //});
-
-
-
-
-
-
-
-  //$(".submitform".val(),.email: $("input[name=email]").val(), 
-  //$(".subscribebutton").on("click", function() {
-   // alert("Thanks for subscribing!");
-  //});
 
 
 
